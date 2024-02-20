6ab921ca0ae4660446eb574a7970cf14be8b010b
cuongpv <cuongpv@intern.vn>
2024-02-20 15:44:42 +0700

    Demo isolate spawn

--- bin/demo_isolate.dart
+++ bin/demo_isolate.dart
@@ -3,8 +3,9 @@ import 'dart:isolate';
 Future<void> main(List<String> arguments) async {
   final startTime = DateTime.now();
 
-  // await testIsolateRun(); // 14ms
-  await testSpawnUri(); // 177ms
+  await testIsolateRun(); // 14ms - about 1ms in release mode
+  // await testSpawnUri(); // 177ms
+  // await testSpawn(); // Same as testIsolateRun
 
   final endTime = DateTime.now();
   print('Duration: ${endTime.difference(startTime).inMilliseconds}ms');
@@ -13,7 +14,7 @@ Future<void> main(List<String> arguments) async {
 Future<void> testIsolateRun() async {
   final result = await Isolate.run(
     () {
-      return 1 + 1;
+      return 42;
     },
   );
 
@@ -23,8 +24,14 @@ Future<void> testIsolateRun() async {
 Future<void> testSpawnUri() async {
   // Spawn an isolate from the given URI
   final receivePort = ReceivePort();
+
+  // Not working after build because dart does not support spawnUri in AOT mode
   final isolate = await Isolate.spawnUri(
-      Uri.parse('isolate_entry.dart'), [], receivePort.sendPort);
+    Uri.parse(
+        'https://raw.githubusercontent.com/pvc97/demo_isolate/master/bin/isolate_entry.dart'),
+    [],
+    receivePort.sendPort,
+  );
 
   // Listen for messages from the spawned isolate
   receivePort.listen((message) {
@@ -34,5 +41,21 @@ Future<void> testSpawnUri() async {
       receivePort.close();
       isolate.kill(priority: Isolate.immediate);
     }
+  });
+}
+
+void isolateFunction(SendPort sendPort) {
+  sendPort.send(42);
+}
+
+Future<void> testSpawn() async {
+  ReceivePort receivePort = ReceivePort();
+
+  Isolate isolate = await Isolate.spawn(isolateFunction, receivePort.sendPort);
+
+  receivePort.listen((message) {
+    print("Received message: $message");
+    receivePort.close();
+    isolate.kill();
   });
 }
